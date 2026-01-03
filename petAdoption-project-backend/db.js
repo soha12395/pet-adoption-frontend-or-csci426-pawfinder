31aad87701915087d0cade504e168ca38c0dbfa8
soha12395 <souhahalawi@gmail.com>
2026-01-03 21:01:25 +0200

    Update server

--- petAdoption-project-backend/db.js
+++ petAdoption-project-backend/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
