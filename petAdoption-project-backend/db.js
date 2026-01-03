f8f4104b8a39d8dbf757f2b32eb969c429976c59
soha12395 <souhahalawi@gmail.com>
2026-01-03 15:28:46 +0200

    Update db.js to use Railway DB and remove local credentials

---
+++ petAdoption-project-backend/db.js
@@ -0,0 +1,22 @@
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+db.connect((err) => {
+  if (err) {
+    console.log("DB connection error:", err);
+  } else {
+    console.log("✅ Connected to Railway DB");
+  }
+});
+
+export default db;
